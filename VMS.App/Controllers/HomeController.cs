4109644efccfb61f98efd81fe2c470b7ae65f375
atkhssn <atik.hassan@outlook.com>
2023-09-24 13:16:00 +0600

    Static login logic implemented

--- VMS.App/Controllers/HomeController.cs
+++ VMS.App/Controllers/HomeController.cs
@@ -1,13 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using VMS.App.Models;
+using VMS.Infrastructure.Model;
 
 namespace VMS.App.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        // static user credentials
+        protected const string Email = "admin@gmail.com";
+        protected const string Password = "admin";
 
+        private readonly ILogger<HomeController> _logger;
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -21,6 +26,22 @@ namespace VMS.App.Controllers
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel loginModel)
+        {
+            var userEmail = loginModel.Email;
+            var userPassword = loginModel.Password;
+
+            if(Email == userEmail & Password == userPassword)
+            {
+                return RedirectToAction("Index", "Admin");
+            }
+            else
+            {
+                return RedirectToAction("Register");
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ VMS.Infrastructure/Model/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VMS.Infrastructure.Model
+{
+    public class LoginModel
+    {
+        public long Id { get; set; }
+        public long UserId { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+
+    }
+}
